3e15ca9fad4e9bd773f3386820077defcdad4114
Cris <crcastro408@gmail.com>
2025-10-06 15:05:42 +0000

    Intial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="./index.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <script>
+    function displayHelp() {
+        
+    }
+    </script>
+    <table>
+        <tr>
+            <th>Chat Room via PHP Web Sockets</th>
+        </tr>
+        <tr>
+            <td>By: Cristopher Castro</td>
+            <td><button onclick="displayHelp()">Help</button></td>
+            <td><a href="">Signup</a></td>
+            <td><a href="">Login</a></td>
+        </tr>
+    <div class="help-container">
+        <div class="help-overlay">
+            
+        </div>
+    </div>
+    </table>
+
+</body>
+</html>
